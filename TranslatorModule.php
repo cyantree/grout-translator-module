72f5509406ecec710533acb5ee533303503a5dc0
cyantree <mail@cyantree.com>
2014-04-13 15:17:41 +0200

    * Initial commit with new versioning

--- TranslatorModule.php
+++ TranslatorModule.php
@@ -5,6 +5,7 @@ use Cyantree\Grout\App\Module;
 use Grout\Cyantree\TranslatorModule\Types\TranslatorConfig;
 use Zend\Cache\Storage\Adapter\Filesystem;
 use Zend\Cache\Storage\Adapter\FilesystemOptions;
+use Zend\Cache\Storage\Plugin\Serializer;
 use Zend\I18n\Translator\Translator;
 
 class TranslatorModule extends Module
@@ -17,16 +18,19 @@ class TranslatorModule extends Module
 
     public function init()
     {
+        $this->app->configs->setDefaultConfig($this->id, new TranslatorConfig(), $this);
+
+        $this->translator = $translator = new Translator();
+
         /** @var TranslatorConfig $config */
-        $this->moduleConfig = $config = $this->app->config->get($this->type, $this->id, new TranslatorConfig());
+        $this->moduleConfig = $config = $this->app->configs->getConfig($this->id);
 
-        $translator = new Translator();
-
-        if(!$this->app->config->developmentMode){
+        if(!$this->app->getConfig()->developmentMode){
             $c = new Filesystem();
             $o = new FilesystemOptions();
             $o->setCacheDir($this->app->cacheStorage->createStorage($this->id));
             $c->setOptions($o);
+            $c->addPlugin(new Serializer());
             $translator->setCache($c);
         }
 
